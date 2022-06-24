602df7e5576ebb1fa1874ac5d6342e8e5eb68a70
Martin Baez <martin.baez@live.com.ar>
2022-06-24 16:03:32 -0300

    agregando for y ifs

--- DesafiosEnGrupo/views.py
+++ DesafiosEnGrupo/views.py
@@ -6,7 +6,10 @@ from django.http import HttpResponse
 
 def mi_template(request):
     nombre = "Martin"
+    apellido = "Baez"
+    
+    listadefamilia = ["Martin", "Mariela", "Marcelo", "Marianela"]
     
     template1 = loader.get_template("mipagina.html")
-    render1 = template1.render({"nombre" : nombre})
+    render1 = template1.render({"nombre" : nombre, "apellido": apellido, "edad": 18, "listafamilia":listadefamilia})
     return HttpResponse(render1)
